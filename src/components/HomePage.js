0fbb146ff647381b639bf70f5e591d1d99658efd
jadens3 <49039591+jadens3@users.noreply.github.com>
2021-03-16 20:08:44 -0700

    Improved Homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-// import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import './HomePage.css';
 
 /**
  * @returns {React.Component} represents the home section
@@ -11,12 +12,12 @@ import Button from 'react-bootstrap/Button';
 const HomePage = () => {
     return (
         <div className='m-5' id='home'>
-            <Container fluid>
+            <Container fluid className='landing'>
                 <Row>
                     <Col className='pb-5'>
-                        <div className='row h-100'>
+                        <div className='row h-100 ml-2'>
                             <div className='col-sm-12 m-auto'>
-                                <h2 className='text-secondary mb-3'>How Well Do You Know Your Legal Rights?</h2>
+                                <h2 className='text-secondary mb-3 mt-3'>How Well Do You Know Your Legal Rights?</h2>
                                 <p className='mb-4'>The law is a powerful tool, but it can be confusing or inaccessible. We're here to help. Our mission is to improve awareness about legal rights and how to exercise them through our youth book and flashcards.</p>
                                 <Button
                                 className='bg-primary'
@@ -30,22 +31,39 @@ const HomePage = () => {
                         </div>
                     </Col>
                     <Col lg className='pb-5'>
-                        {/*
-                        <Carousel>
+                        <Carousel className='pt-5' controls={false}>
                             <Carousel.Item>
-                                <img
+                                <div className='carousel-image-container'>
+                                    <img
                                     src='https://i.ibb.co/cb2vd5L/preordernow.jpg'
-                                    alt='first slide'
-                                    className='d-block w-100'
-                                />
+                                    alt='preorder now'
+                                    className='d-block carousel-image'
+                                    />
+                                </div>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <div className='carousel-image-container'>
+                                    <a href='#/flashcards'>
+                                        <img
+                                        src='https://i.ibb.co/MPqv6N8/Flashcards.png'
+                                        alt='preorder now'
+                                        className='d-block carousel-image'
+                                        />
+                                    </a>
+                                </div>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <div className='carousel-image-container'>
+                                    <a href='#/book'>
+                                        <img
+                                        src='https://i.ibb.co/5WjgWPB/SeeBooks.png'
+                                        alt='preorder now'
+                                        className='d-block carousel-image'
+                                        />
+                                    </a>
+                                </div>
                             </Carousel.Item>
                         </Carousel>
-                        */}
-                        <img
-                            src='https://i.ibb.co/cb2vd5L/preordernow.jpg'
-                            alt='preorder now'
-                            className='d-block w-100'
-                        />
                     </Col>
                 </Row>
             </Container>
