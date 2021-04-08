650539d616884bcf12d9376f34396f4819d7fc3c
jadens3 <49039591+jadens3@users.noreply.github.com>
2021-04-08 15:01:17 -0700

    Implemented new navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Banner from './Banner.js';
 import './Navigation.css';
 
 /**
@@ -9,16 +8,12 @@ import './Navigation.css';
  */
 const Navigation = () => {
     return (
-        <div className='mb-5'>
-            <Navbar expand='lg' className='bg-primary' variant='dark'>
-                <Navbar.Brand href='/'>
-                <div className='logo-container'>
-                        <img src="https://i.ibb.co/wNtwVV4/logo.png" alt="logo" border="0"/>
-                </div>
-                </Navbar.Brand>
+        <header className='bg-primary text-white'>
+            <h1>Law+</h1>
+            <Navbar expand='lg' variant='dark'>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className='mr-auto'>
+                    <Nav>
                         <Nav.Item>
                             <Nav.Link href='#/' className='text-white'>Home</Nav.Link>
                         </Nav.Item>
@@ -29,13 +24,12 @@ const Navigation = () => {
                             <Nav.Link href='#/book' className='text-white'>Book</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href='#/team' className='text-white'>Team</Nav.Link>
+                            <Nav.Link href='#/team' className='text-white'>About</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-            <Banner />
-        </div>
+        </header>
     );
 }
 
