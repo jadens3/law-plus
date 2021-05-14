f812dc8e98ba064a8f98e04ca27a62733782d496
jadens3 <49039591+jadens3@users.noreply.github.com>
2021-05-14 13:08:33 -0700

    Updated time on banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 const Banner = () => {
   return (
     <section className="text-primary text-center" id="banner">
-      <h2>Check out our pop-up event on Sunday, 4/16 from 1PM-4PM PST at <a href="https://www.coneandsteiner.com/">
+      <h2>Check out our pop-up event on Sunday, 4/16 from 11AM-2PM PST at <a href="https://www.coneandsteiner.com/">
           Cone &#38; Steiner
         </a> in Pioneer Square!
       </h2>
