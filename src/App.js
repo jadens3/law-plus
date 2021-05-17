5b799d7afcabe6a89a938ad557df48a934db9182
jadens3 <49039591+jadens3@users.noreply.github.com>
2021-05-17 16:57:11 -0700

    Removed sales event banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
 import Navigation from './components/Navigation.js';
-import Banner from './components/Banner.js';
 import HomePage from './components/HomePage.js';
 import AboutPage from './components/AboutPage.js';
 import FlashcardsPage from './components/FlashcardsPage.js';
@@ -12,7 +11,6 @@ function App() {
   return (
     <HashRouter>
       <Navigation/>
-      <Banner />
       <main>
         <Switch>
           <Route path='/' exact><HomePage id="home"/></Route>
